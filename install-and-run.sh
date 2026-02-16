05caf161c7ba49b2d21ab34feda03eae69b4ba3e
Deniz issi <denizz.services@gmail.com>
2026-02-16 18:56:50 +0300

    Update install-and-run.sh

--- install-and-run.sh
+++ install-and-run.sh
@@ -19,7 +19,7 @@ print_jishnu_logo() {
     echo "                 | |__| | \__ \ | | | | | | |_| |"
     echo "                  \____/|_|___/_| |_|_| |_|\__,_|"
     echo "                                                  "
-    echo "                    POWERED BY DENIZTECH          "
+    echo "                    POWERED BY DENİ"
     echo "========================================================================"
     echo -e "${N}"
 }
